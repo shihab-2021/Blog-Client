93cc50fc9149d97b37c46e9d1122907a209b3008
shihab-2021 <shihab77023@gmail.com>
2025-04-27 21:00:42 +0600

    update

--- src/app/(WithCommonLayout)/(home)/page.js
+++ src/app/(WithCommonLayout)/(home)/page.js
@@ -1,3 +1,4 @@
+import HeroSection from "@/components/home/HeroSection";
 import Footer from "@/components/shared/Footer";
 import Navbar from "@/components/shared/Navbar";
 import Image from "next/image";
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <HeroSection />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
           <Image
